fc3446d35bb392ca73109b4d4fff1f86dcb436e5
MattJaegerCO <104642956+MattJaegerCO@users.noreply.github.com>
2022-04-29 10:58:56 -0600

    Update raspberry_pi_os_stable_berryboot.sh
    Updating source links to RaspberryPi.com
    Updated unzip to unxz
    Fixed Names, ETC.
    Removed All command, too much effort lol

--- os-selection/raspberry-pi-os/raspberry_pi_os_stable_berryboot.sh
+++ os-selection/raspberry-pi-os/raspberry_pi_os_stable_berryboot.sh
@@ -26,14 +26,14 @@ echo "             |_|                     |__/                     ";
 echo "--------------------------------------------------------------"
 
 #Download URLs
-URL1="https://berryboot.alexgoldcheidt.com/go/raspberry-pi-os-full-desktop"
-URL2="https://berryboot.alexgoldcheidt.com/go/raspberry-pi-os-basic-desktop"
-URL3="https://berryboot.alexgoldcheidt.com/go/raspberry-pi-os-lite"
+URL1="https://downloads.raspberrypi.org/raspios_arm64/images/raspios_arm64-2022-04-07/2022-04-04-raspios-bullseye-arm64.img.xz"
+URL2="https://downloads.raspberrypi.org/raspios_armhf/images/raspios_armhf-2022-04-07/2022-04-04-raspios-bullseye-armhf.img.xz"
+URL3="https://downloads.raspberrypi.org/raspios_full_armhf/images/raspios_full_armhf-2022-04-07/2022-04-04-raspios-bullseye-armhf-full.img.xz"
 
 #Names for Converted OS Images
-NAME1="raspberry_pi_os_latest_full_desktop_berryboot-$date.img"
-NAME2="raspberry_pi_os_latest_basic_desktop_berryboot-$date.img"
-NAME3="raspberry_pi_os_latest_lite_berryboot-$date.img"
+NAME1="Raspberry Pi_OS_with_desktop_April42022_64BIT_Bullseye-$date.img"
+NAME2="Raspberry Pi_OS_with_desktop_April42022_32BIT_Bullseye-$date.img"
+NAME3="Raspberry Pi_OS_with_desktop_and_rec_soft_April42022_32BIT_Bullseye-$date.img"
 
 #Mount Points
 MNT1="/mnt/raspberry-pi-os-boot"
@@ -45,23 +45,23 @@ echo ""
 
 #Raspberry Pi OS Image Menu Selection
 PS3='Please select your device: '
-options=("Raspberry Pi OS Full Desktop" "Raspberry Pi OS Basic Desktop" "Raspberry Pi OS Lite" "All Raspberry Pi OS Builds" "Exit")
+options=("Raspberry Pi OS 64BIT Desktop" "Raspberry Pi OS Desktop 32BIT" "Raspberry Pi OS Desktop and Recommended Software 32BIT" "Exit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Raspberry Pi OS Full Desktop")
+        "Raspberry Pi OS 64BIT Desktop")
 		
 echo ""
-echo "#### DOWNLOADING RASPBERRY PI OS FULL DESKTOP IMAGE ####"
+echo "#### DOWNLOADING RASPBERRY PI OS 64BIT DESKTOP IMAGE ####"
 echo ""
            aria2c -x 4 -s 4 "$URL1"
 			clear
 echo ""
-echo "#### DECOMPRESSING RASPBERRY PI OS FULL DESKTOP IMAGE ####"
+echo "#### DECOMPRESSING RASPBERRY PI OS 64BIT DESKTOP IMAGE ####"
 echo ""
-			sudo unzip *armhf-full.zip
+			sudo unxz --threads=0 *arm64.img.xz
 			sudo mkdir $MNT1 $MNT2
-			sudo losetup loop55 -P *armhf-full.img
+			sudo losetup loop55 -P *arm64.img
 			sudo mount /dev/loop55p1 $MNT1
 			sudo mount /dev/loop55p2 $MNT2
 			sudo find $MNT2 -name 'cached_UTF-8_del.kmap.gz' -exec sh -c 'rm -f $1' _ {} \;
@@ -72,15 +72,15 @@ echo ""
 			sudo cp -R $MNT1/* $MNT2/boot/
 clear
 echo ""
-echo "#### CONVERTING RASPBERRY PI OS FULL DESKTOP IMAGE TO BERRYBOOT ####"
+echo "#### CONVERTING RASPBERRY PI OS 64BIT DESKTOP IMAGE TO BERRYBOOT ####"
 echo ""
 			sudo mksquashfs $MNT2 $NAME1 -comp lzo -e lib/modules var/cache/apt/archives var/lib/apt/lists
 			sudo umount $MNT1 $MNT2
 			sudo losetup -d /dev/loop55
-			sudo rm -rf *armhf-full* $MNT1 $MNT2
+			sudo rm -rf *arm64* $MNT1 $MNT2
 			clear
 echo ""
-echo "#### RASPBERRY PI OS FULL DESKTOP IMAGE READY! ####"
+echo "#### RASPBERRY PI OS 64BIT DESKTOP IMAGE READY! ####"
 echo ""
 echo "-----------------------------------------------";
 echo "Support my project at: paypal.me/alexgoldc";
@@ -88,17 +88,17 @@ echo "-----------------------------------------------";
 echo ""
 			break
             ;;
-	"Raspberry Pi OS Basic Desktop")
+	"Raspberry Pi OS Desktop 32BIT")
 		
 echo ""
-echo "#### DOWNLOADING RASPBERRY PI OS BASIC DESKTOP IMAGE ####"
+echo "#### DOWNLOADING RASPBERRY PI OS DESKTOP 32BIT IMAGE ####"
 echo ""
            aria2c -x 4 -s 4 "$URL2"
 			clear
 echo ""
-echo "#### DECOMPRESSING RASPBERRY PI OS BASIC DESKTOP IMAGE ####"
+echo "#### DECOMPRESSING RASPBERRY PI OS DESKTOP 32 BIT IMAGE ####"
 echo ""
-			sudo unzip *armhf.zip
+			sudo unxz --threads=0 *armhf.img.xz
 			sudo mkdir $MNT1 $MNT2
 			sudo losetup loop55 -P *armhf.img
 			sudo mount /dev/loop55p1 $MNT1
@@ -111,7 +111,7 @@ echo ""
 			sudo cp -R $MNT1/* $MNT2/boot/
 clear
 echo ""
-echo "#### CONVERTING RASPBERRY PI OS BASIC DESKTOP IMAGE TO BERRYBOOT ####"
+echo "#### CONVERTING RASPBERRY PI OS DESKTOP 32BIT IMAGE TO BERRYBOOT ####"
 echo ""
 			sudo mksquashfs $MNT2 $NAME2 -comp lzo -e lib/modules var/cache/apt/archives var/lib/apt/lists
 			sudo umount $MNT1 $MNT2
@@ -119,7 +119,7 @@ echo ""
 			sudo rm -rf *armhf* $MNT1 $MNT2
 			clear
 echo ""
-echo "#### RASPBERRY PI OS BASIC DESKTOP IMAGE READY! ####"
+echo "#### RASPBERRY PI OS DESKTOP 32BIT IMAGE READY! ####"
 echo ""
 echo "-----------------------------------------------";
 echo "Support my project at: paypal.me/alexgoldc";
@@ -127,56 +127,17 @@ echo "-----------------------------------------------";
 echo ""
 			break
             ;;
-	"Raspberry Pi OS Lite")
+	"Raspberry Pi OS Desktop and Recommended Software 32BIT")
 		
 echo ""
-echo "#### DOWNLOADING RASPBERRY PI OS LITE IMAGE ####"
+echo "#### DOWNLOADING Raspberry Pi OS Desktop and Recommended Software 32BIT IMAGE ####"
 echo ""
            aria2c -x 4 -s 4 "$URL3"
 			clear
 echo ""
-echo "#### DECOMPRESSING RASPBERRY PI OS LITE IMAGE ####"
+echo "#### DECOMPRESSING Raspberry Pi OS Desktop and Recommended Software 32BIT IMAGE ####"
 echo ""
-			sudo unzip *armhf-lite.zip
-			sudo mkdir $MNT1 $MNT2
-			sudo losetup loop55 -P *armhf-lite.img
-			sudo mount /dev/loop55p1 $MNT1
-			sudo mount /dev/loop55p2 $MNT2
-			sudo find $MNT2 -name 'cached_UTF-8_del.kmap.gz' -exec sh -c 'rm -f $1' _ {} \;
-			sudo find $MNT2 -name 'apply_noobs_os_config.service' -exec sh -c 'rm -f $1' _ {} \;
-			sudo find $MNT2 -name 'raspberrypi-net-mods.service' -exec sh -c 'rm -f $1' _ {} \;
-			sudo sed -i 's/^\PARTUUID/#\0/g' $MNT2/etc/fstab
-			sudo rm -f $MNT1/kernel* $MNT1/*.elf
-			sudo cp -R $MNT1/* $MNT2/boot/
-clear
-echo ""
-echo "#### CONVERTING RASPBERRY PI OS LITE IMAGE TO BERRYBOOT ####"
-echo ""
-			sudo mksquashfs $MNT2 $NAME3 -comp lzo -e lib/modules var/cache/apt/archives var/lib/apt/lists
-			sudo umount $MNT1 $MNT2
-			sudo losetup -d /dev/loop55
-			sudo rm -rf *armhf-lite* $MNT1 $MNT2
-			clear
-echo ""
-echo "#### RASPBERRY PI OS LITE IMAGE READY! ####"
-echo ""
-echo "-----------------------------------------------";
-echo "Support my project at: paypal.me/alexgoldc";
-echo "-----------------------------------------------";
-echo ""
-			break
-            ;;			
-        "All Raspberry Pi OS Builds")
-		
-echo ""
-echo "#### DOWNLOADING RASPBERRY PI OS FULL DESKTOP IMAGE ####"
-echo ""
-           aria2c -x 4 -s 4 "$URL1"
-			clear
-echo ""
-echo "#### DECOMPRESSING RASPBERRY PI OS FULL DESKTOP IMAGE ####"
-echo ""
-			sudo unzip *armhf-full.zip
+			sudo unxz --threads=0 *armhf-full.img.xz
 			sudo mkdir $MNT1 $MNT2
 			sudo losetup loop55 -P *armhf-full.img
 			sudo mount /dev/loop55p1 $MNT1
@@ -189,71 +150,15 @@ echo ""
 			sudo cp -R $MNT1/* $MNT2/boot/
 clear
 echo ""
-echo "#### CONVERTING RASPBERRY PI OS FULL DESKTOP IMAGE TO BERRYBOOT ####"
+echo "#### CONVERTING Raspberry Pi OS Desktop and Recommended Software 32BIT IMAGE TO BERRYBOOT ####"
 echo ""
-			sudo mksquashfs $MNT2 $NAME1 -comp lzo -e lib/modules var/cache/apt/archives var/lib/apt/lists
+			sudo mksquashfs $MNT2 $NAME3 -comp lzo -e lib/modules var/cache/apt/archives var/lib/apt/lists
 			sudo umount $MNT1 $MNT2
 			sudo losetup -d /dev/loop55
 			sudo rm -rf *armhf-full* $MNT1 $MNT2
 			clear
 echo ""
-echo "#### DOWNLOADING RASPBERRY PI OS BASIC DESKTOP IMAGE ####"
-echo ""
-           aria2c -x 4 -s 4 "$URL2"
-			clear
-echo ""
-echo "#### DECOMPRESSING RASPBERRY PI OS BASIC DESKTOP IMAGE ####"
-echo ""
-			sudo unzip *armhf.zip
-			sudo mkdir $MNT1 $MNT2
-			sudo losetup loop55 -P *armhf.img
-			sudo mount /dev/loop55p1 $MNT1
-			sudo mount /dev/loop55p2 $MNT2
-			sudo find $MNT2 -name 'cached_UTF-8_del.kmap.gz' -exec sh -c 'rm -f $1' _ {} \;
-			sudo find $MNT2 -name 'apply_noobs_os_config.service' -exec sh -c 'rm -f $1' _ {} \;
-			sudo find $MNT2 -name 'raspberrypi-net-mods.service' -exec sh -c 'rm -f $1' _ {} \;
-			sudo sed -i 's/^\PARTUUID/#\0/g' $MNT2/etc/fstab
-			sudo rm -f $MNT1/kernel* $MNT1/*.elf
-			sudo cp -R $MNT1/* $MNT2/boot/
-clear
-echo ""
-echo "#### CONVERTING RASPBERRY PI OS BASIC DESKTOP IMAGE TO BERRYBOOT ####"
-echo ""
-			sudo mksquashfs $MNT2 $NAME2 -comp lzo -e lib/modules var/cache/apt/archives var/lib/apt/lists
-			sudo umount $MNT1 $MNT2
-			sudo losetup -d /dev/loop55
-			sudo rm -rf *armhf* $MNT1 $MNT2
-			clear
-echo ""
-echo "#### DOWNLOADING RASPBERRY PI OS LITE IMAGE ####"
-echo ""
-           aria2c -x 4 -s 4 "$URL3"
-			clear
-echo ""
-echo "#### DECOMPRESSING RASPBERRY PI OS LITE IMAGE ####"
-echo ""
-			sudo unzip *armhf-lite.zip
-			sudo mkdir $MNT1 $MNT2
-			sudo losetup loop55 -P *armhf-lite.img
-			sudo mount /dev/loop55p1 $MNT1
-			sudo mount /dev/loop55p2 $MNT2
-			sudo find $MNT2 -name 'cached_UTF-8_del.kmap.gz' -exec sh -c 'rm -f $1' _ {} \;
-			sudo find $MNT2 -name 'apply_noobs_os_config.service' -exec sh -c 'rm -f $1' _ {} \;
-			sudo find $MNT2 -name 'raspberrypi-net-mods.service' -exec sh -c 'rm -f $1' _ {} \;
-			sudo sed -i 's/^\PARTUUID/#\0/g' $MNT2/etc/fstab
-			sudo rm -f $MNT1/kernel* $MNT1/*.elf
-			sudo cp -R $MNT1/* $MNT2/boot/
-clear
-echo ""
-echo "#### CONVERTING RASPBERRY PI OS LITE IMAGE TO BERRYBOOT ####"
-echo ""
-			sudo mksquashfs $MNT2 $NAME3 -comp lzo -e lib/modules var/cache/apt/archives var/lib/apt/lists
-			sudo umount $MNT1 $MNT2
-			sudo losetup -d /dev/loop55
-			sudo rm -rf *armhf-lite* $MNT1 $MNT2
-			clear
-echo ""
-echo "#### ALL RASPBERRY PI OS IMAGES READY! ####"
+echo "#### Raspberry Pi OS Desktop and Recommended Software 32BIT IMAGE READY! ####"
 echo ""
 echo "-----------------------------------------------";
 echo "Support my project at: paypal.me/alexgoldc";
