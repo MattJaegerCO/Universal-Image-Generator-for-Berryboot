1bd4f7bc8998c39d3a422ee139d8063a141ed9f6
MattJaegerCO <104642956+MattJaegerCO@users.noreply.github.com>
2022-04-29 11:17:50 -0600

    Update universal_image_generator_berryboot.sh

--- universal_image_generator_berryboot.sh
+++ universal_image_generator_berryboot.sh
@@ -131,12 +131,4 @@ bash <(wget -qO- https://raw.githubusercontent.com/MattJaegerCO/Universal-Image-
             ;;
         *) echo invalid option;;
     esac
-	echo "Press any key to continue"
-	while [ true ] ; do
-		read -t 3 -n 1
-		if [ $? = 0 ] ; then
-		exit ;
-	else
-		echo "waiting for the keypress"
-	fi
 done
