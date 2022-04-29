485c31b7d1f1ab2e5b1bf42dfc81adb62137699a
MattJaegerCO <104642956+MattJaegerCO@users.noreply.github.com>
2022-04-29 11:14:51 -0600

    Update raspberry_pi_os_stable_berryboot.sh

--- os-selection/raspberry-pi-os/raspberry_pi_os_stable_berryboot.sh
+++ os-selection/raspberry-pi-os/raspberry_pi_os_stable_berryboot.sh
@@ -157,18 +157,16 @@ echo ""
 			break
             ;;			
         "Exit")
-	
-	echo "Press any key to continue"
-	while [ true ] ; do
-	read -t 3 -n 1
-	if [ $? = 0 ] ; then
-	exit ;
-	else
-	echo "waiting for the keypress"
-	fi
-        
 	break
             ;;
         *) echo invalid option;;
     esac
+    	echo "Press any key to continue"
+	while [ true ] ; do
+		read -t 3 -n 1
+		if [ $? = 0 ] ; then
+		exit ;
+	else
+		echo "waiting for the keypress"
+	fi
 done
